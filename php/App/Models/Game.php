ee95bca5745deeda18d55fcd5ce1a94ccaf6483b
Kokon4 <kicodbz1@gmail.com>
2024-10-21 19:48:28 +0200

    Carpetes creades

---
+++ php/App/Models/Game.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Joc4enRatlla\Models;
+
+use Joc4enRatlla\Models\Board;
+use Joc4enRatlla\Models\Player;
+
+class Game
+{
+    private Board $board;
+    private int $nextPlayer;
+    private array $players;
+    private ?Player $winner;
+    private array $scores = [1 => 0, 2 => 0];
+
+    //public function __construct( Player $jugador1, Player $jugador2);
+
+    // getters i setters
+
+    //public function reset(): void //Reinicia el joc
+    //public function play($columna)  //Realitza un moviment
+    //public function playAutomatic(){copiarDelsApunts}
+    //public function save()  //Guarda l'estat del joc a les sessions
+    //public static function restore() //Restaura l'estat del joc de les sessions
+}
+
+
+?>
